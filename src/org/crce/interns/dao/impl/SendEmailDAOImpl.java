f3d758946c629241fca7372efcc79fa2e72c5c9c
LeonDsouza <leonsurajd69@gmail.com>
2016-05-28 16:01:30 +0530

    Update SendEmailDAOImpl.java

--- src/org/crce/interns/dao/impl/SendEmailDAOImpl.java
+++ src/org/crce/interns/dao/impl/SendEmailDAOImpl.java
@@ -3,6 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+* @author Leon
+* Task: Communicates with database to get group values
+* Dependency: -
+*/
 package org.crce.interns.dao.impl;
 
 import java.util.Arrays;
