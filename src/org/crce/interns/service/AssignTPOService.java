b943a4707a063365230ff35ee3f56c3e8299f3f3
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-06-01 00:06:38 +0530

    commited locally ZNevzz

--- src/org/crce/interns/service/AssignTPOService.java
+++ src/org/crce/interns/service/AssignTPOService.java
@@ -14,6 +14,7 @@ package org.crce.interns.service;
 
 import java.util.List;
 
+import org.crce.interns.beans.ProfessionalProfileBean;
 import org.crce.interns.beans.UserDetailsBean;
 import org.crce.interns.model.UserDetails;
 import org.springframework.stereotype.Service;
