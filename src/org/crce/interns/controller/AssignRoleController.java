2f52b031141736a137c7c9e617d8a4f9bff6fc2b
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-06-07 08:31:29 +0530

    solved conflicts by @ZNevzz 2

--- src/org/crce/interns/controller/AssignRoleController.java
+++ src/org/crce/interns/controller/AssignRoleController.java
@@ -7,16 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+
+
 import org.crce.interns.beans.AssignRoleBean;
 import org.crce.interns.model.FunctionMaster;
 import org.crce.interns.model.FunctionRole;
 import org.crce.interns.service.AssignRoleService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+
 @Controller
 public class AssignRoleController {
 	@Autowired
