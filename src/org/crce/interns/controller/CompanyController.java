cbcc0820f864c2b72cdad6c61541baef992cbce0
AnuIssac <anuissac1202@gmail.com>
2016-06-13 11:26:21 +0530

    Company contoller try catch added

--- src/org/crce/interns/controller/CompanyController.java
+++ src/org/crce/interns/controller/CompanyController.java
@@ -46,22 +46,35 @@ public class CompanyController {
 	 @RequestMapping(value = "/addCompany", method = RequestMethod.GET)
 		public ModelAndView addCompany(Model model) {
 		 
+		 try{
 		 CompanyBean companyBean=new CompanyBean();
 		 model.addAttribute("companyBean",companyBean);
 			System.out.println("in controller1");
 			return new ModelAndView("addCompany");
+			
+		 }
+		 catch(Exception e){
+				System.out.println(e);
+
+				ModelAndView model1=new ModelAndView("500");
+				model1.addObject("message", "Your session has timed out. Please login again");
+		 		//model.addObject("url", "form");
+
+				return model1;
+			}
+
 		}
 	 @RequestMapping(value = "/saveCompany", method = RequestMethod.POST)
 		public ModelAndView saveCompany(  @ModelAttribute("companyBean") CompanyBean companyBean, 
 				BindingResult result) {
 		 
-		 
+		 try{
 		 companyValidator.validate(companyBean, result);
 			if (result.hasErrors()) {
 		System.out.println("Error in form");
      
      return new ModelAndView("addCompany");
- }
+         }
 			Company company = prepareCompanyModel(companyBean);
 			
 			companyService.addCompany(company);
@@ -69,7 +82,19 @@ public class CompanyController {
 //			return new ModelAndView("companysuccess");
 
 			return new ModelAndView("redirect:/addCompany");
+         
+		
+	 }
+	 catch(Exception e){
+			System.out.println(e);
 
+			ModelAndView model1=new ModelAndView("500");
+			model1.addObject("message", "Your session has timed out. Please login again");
+	 		model1.addObject("url", "form");
+
+			return model1;
 		}
 
+
+}
 }
