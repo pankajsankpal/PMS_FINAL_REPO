e332b5e0d639dac659fe96f764db8c762a3d5d77
frcrceinterns <frcrcecompsinterns@gmail.com>
2016-06-14 14:27:21 +0530

    Add files via upload

---
+++ src/org/crce/interns/dao/GetCompaniesDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.crce.interns.dao;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Leon
+ */
+public interface GetCompaniesDAO {
+    public Map getCompanies();
+}
