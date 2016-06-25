6f8d08ded1c1535ee3156da5e5e938ff828c7b9b
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-06-25 11:48:25 +0530

    corrected get criteria bean list

--- src/org/crce/interns/dao/impl/CriteriaDaoImpl.java
+++ src/org/crce/interns/dao/impl/CriteriaDaoImpl.java
@@ -1,5 +1,6 @@
 package org.crce.interns.dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.crce.interns.beans.CriteriaBean;
@@ -7,6 +8,7 @@ import org.crce.interns.dao.CriteriaDao;
 import org.crce.interns.model.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
@@ -45,6 +47,20 @@ public class CriteriaDaoImpl implements CriteriaDao {
 
 	@SuppressWarnings("unchecked")
 	public List<CriteriaBean> getCriteria() {  
-		  return (List<CriteriaBean>) sessionFactory.getCurrentSession().createCriteria(CriteriaBean.class).list();  
+		  //return (List<CriteriaBean>) sessionFactory.getCurrentSession().createCriteria(CriteriaBean.class).list();
+		
+			List<Criteria> crit = sessionFactory.getCurrentSession().createCriteria(Criteria.class).list();
+			List<CriteriaBean> result = new ArrayList<CriteriaBean>(); 
+			
+			for(Criteria i: crit){
+				
+				CriteriaBean temp = new CriteriaBean();
+				
+				BeanUtils.copyProperties(i, temp);
+				
+				result.add(temp);
+			}
+			
+			return result;
 		 }  
 }
