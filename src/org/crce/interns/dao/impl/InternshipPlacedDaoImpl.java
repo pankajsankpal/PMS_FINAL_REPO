bf6a31cbdc183617b42656622dfa4122e25d4f4e
AnuIssac <anuissac1202@gmail.com>
2016-07-09 15:36:41 +0530

    changes in stats

--- src/org/crce/interns/dao/impl/InternshipPlacedDaoImpl.java
+++ src/org/crce/interns/dao/impl/InternshipPlacedDaoImpl.java
@@ -1,55 +1,38 @@
-package org.crce.interns.dao.impl;
+package org.crce.interns.service.impl;
 
-import java.util.Calendar;
 import java.util.List;
 
 import org.crce.interns.beans.InternshipPlacedBean;
+import org.crce.interns.dao.HigherStudiesDao;
 import org.crce.interns.dao.InternshipPlacedDao;
-import org.crce.interns.model.Feedback;
 import org.crce.interns.model.InterestedInHigherStudies;
 import org.crce.interns.model.InternshipPlaced;
-import org.hibernate.Query;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.BeanUtils;
+import org.crce.interns.service.InternshipPlacedService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-@Repository("internshipplacedDao")
+@Service("internshipplacedService")
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 
-public class InternshipPlacedDaoImpl implements InternshipPlacedDao {
+public class InternshipPlacedServiceImpl implements InternshipPlacedService {
 
 	@Autowired
-	private SessionFactory sessionFactory;
+	private InternshipPlacedDao hsDao;
 	
+	
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public void addIP(InternshipPlacedBean ihs) {
-		InternshipPlaced ihss=new 	InternshipPlaced();
-		
-		BeanUtils.copyProperties(ihs,ihss);
-		System.out.println("&&&&&&"+ihss.getYear());
-		System.out.println(ihs.getYear()+"&&&&&&&");
-		sessionFactory.getCurrentSession().save(ihss);
-	}
+		hsDao.addIP(ihs);
 	
-	@SuppressWarnings("unchecked")
-	public List<InternshipPlaced> listIhs() {
-List<InternshipPlaced> list = null;
-		
-
-
-		
-		
-		String y = Integer.toString(Calendar.getInstance().get(Calendar.YEAR));
-		
-		Query query = sessionFactory.getCurrentSession()
-				.createQuery("SELECT U FROM InternshipPlaced U WHERE U.year = :curYear");
-		query.setParameter("curYear", y);
-		list = query.list();
-		
-		System.out.println("inside dao size of list = " + list.size());
-		
-		return list;
+}
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+	
+	public InternshipPlaced listIhs(String year) {
+		System.out.println("inside service");
+		return hsDao.listIhs(year);
 	}
-
-
 
 }
