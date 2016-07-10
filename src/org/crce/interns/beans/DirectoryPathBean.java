5c9e233e95c53b574321e67fded3e4e6571fcfb1
melwyn95 <melwyn95@gmail.com>
2016-07-10 14:04:44 +0530

    working

--- src/org/crce/interns/beans/DirectoryPathBean.java
+++ src/org/crce/interns/beans/DirectoryPathBean.java
@@ -23,7 +23,7 @@ public class DirectoryPathBean {
     String year = Integer.toString(Calendar.getInstance().get(Calendar.YEAR))+"-"+ Integer.toString(Calendar.getInstance().get(Calendar.YEAR)+1);
 
     //private final String rootContext="C:\\PMS\\"+year;
-    private final String rootContext="home/universe/PMS/"+year;
+    private final String rootContext="/home/melwyn/PMS/"+year;
     private final String studentFolder=rootContext + "/Users/Student";
     private final String facultyFolder=rootContext + "/Users/Faculty";
     private final String tpoFolder = rootContext + "/Users/TPO";
